232467f6492d77d524b6147d740b64fb3444b12b
zh1 <834197886@qq.com>
2024-08-26 16:52:21 +0800

    feat:引入element-plus 并且定义style

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import App from './App.vue'
 const app = createApp(App);
 
 // 样式
-import './style/style.css'
-import './style/common/common.scss'
+import '@/style/common/common.scss'
+import '@/style/main.scss'
+import 'element-plus/dist/index.css'
+
 
 // 组件
 import { registerGlobalComponents } from '@/components/index';
 // 动态注册全局组件
 registerGlobalComponents(app);
+import ElementPlus from 'element-plus'
 
+app.use(ElementPlus)
 app.mount('#app')
